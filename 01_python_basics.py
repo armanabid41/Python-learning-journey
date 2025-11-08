df988fa4e037adcbd9524767dc85c2058c1784c1
Arman Hossain Abid <armanabid41@gmail.com>
2025-11-09 00:48:54 +0600

    Delete 01_python_basics.py

--- 01_python_basics.py
+++
@@ -1,9 +0,0 @@
-# variable type
-# List (list): Ordered, mutable (changeable)
-marks = [85, 90.1, 78, 'A']
-print("Original list:", marks)
-
-marks[0] = 95 # list can be modified
-print("Modified list:", marks)
-
-print("The type is:", type(marks))
